a1e33ee3eda1a54650903bcee2385d1974559a58
BrenoHenrique07 <brenohenriquess123@gmail.com>
2022-10-07 02:20:25 -0300

    Adicionando mini historico e valores flutuantes

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -9,66 +9,68 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textOutput: TextView
+    private lateinit var textOutHistory: TextView
     private var concat: String = " "
     private var operationAux: String = " "
+    private var history: String = " "
     private var numberop: Double = 0.0
     private var result: Double = 0.0
+    private var numDouble: Double = 0.0
     private var modeResult: Boolean = false
+    private var modePoint: Boolean = false
+    private var flagPoints: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         this.textOutput = findViewById(R.id.textView)
+        this.textOutHistory = findViewById(R.id.textView2)
     }
 
     fun onClickNumber(view: View) {
         val valButton = view as Button
 
         this.concat += valButton.text.toString()
+
+        if(this.modePoint) {
+            this.numDouble = this.concat.toDouble()/10
+            this.concat = this.numDouble.toString()
+            this.modePoint = false
+        }
+
         this.textOutput.text = this.concat
     }
 
     fun onClickOperation(view: View) {
         val operation = view as Button
+        val r: Double
 
         if(this.concat.length > 1) {
+
             this.numberop = concat.toDouble()
 
             if(!modeResult) {
                 this.result = this.numberop
-                this.operationAux = operation.text.toString()
-
                 this.modeResult = true
             } else {
-                calcHistory(this.numberop, this.operationAux)
-                this.operationAux = operation.text.toString()
+                selectOperation(this.numberop, this.operationAux)
             }
 
             this.concat = " "
             this.numberop = 0.0
+            r = this.result
 
             if(operation.text == "="){
                 result()
             }
         } else {
+            r = this.result
             this.textOutput.text = this.result.toString()
         }
-
-    }
-
-    private fun calcHistory(number1: Double, operation: String) {
-
-        when (operation) {
-            "+" -> this.result = add(number1, this.result)
-            "-" -> this.result = sub(number1, this.result)
-            "X" -> this.result = mul(number1, this.result)
-            "/" -> this.result = div(number1, this.result)
-
-            else -> 0.0
-        }
-
-        this.textOutput.text = this.result.toString()
+        this.flagPoints = true
+        this.operationAux = operation.text.toString()
+        calcHistory(r, operation.text.toString())
     }
 
     fun onClickDeleteAll(view: View) {
@@ -84,9 +86,43 @@ class MainActivity : AppCompatActivity() {
         } else {
             this.concat = " "
             this.result = 0.0
+            this.textOutHistory.text = " "
         }
 
         this.textOutput.text = this.result.toString()
+    }
+
+    fun onClickPoint(view: View) {
+
+        if(this.flagPoints) {
+            if(this.concat.length > 1){
+                this.modePoint = !this.modePoint
+                this.flagPoints = false
+            } else {
+                this.textOutput.text = this.result.toString()
+            }
+        }
+    }
+
+    private fun selectOperation(number1: Double, operation: String) {
+
+        when (operation) {
+            "+" -> this.result = add(number1, this.result)
+            "-" -> this.result = sub(number1, this.result)
+            "X" -> this.result = mul(number1, this.result)
+            "/" -> this.result = div(number1, this.result)
+
+            else -> 0.0
+        }
+
+        calcHistory(this.result, operation)
+        this.textOutput.text = this.result.toString()
+    }
+
+    private fun calcHistory(number: Double, s: String) {
+        this.history = " "
+        this.history += "$number $s"
+        this.textOutHistory.text = this.history
     }
 
     private fun add(nm1 : Double, nm2 : Double): Double {return nm1 + nm2}
@@ -94,8 +130,10 @@ class MainActivity : AppCompatActivity() {
     private fun mul(nm1 : Double, nm2 : Double): Double {return nm1 * nm2}
     private fun div(nm1 : Double, nm2 : Double): Double {return nm2 / nm1}
     private fun result() {  this.concat = " "
-        this.numberop = 0.0
-        this.operationAux = " "
-        this.result = 0.0
-        this.modeResult = false}
+                            this.numberop = 0.0
+                            this.operationAux = " "
+                            this.textOutHistory.text = " "
+                            this.result = 0.0
+                            this.modeResult = false
+    }
 }
